aff163617095ec12775432ea54a799c74036faa4
Clodoege <clodoege@gmail.com>
2024-03-07 20:25:10 -0300

    primeiro commit

---
+++ adm/inc/footer.inc.php
@@ -0,0 +1,6 @@
+<footer>
+        <h1>Aqui será o meu rodapé </h1>
+    </footer>
+
+</body>
+</html>
---
+++ adm/inc/header.inc.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+    content="width=, initial-scale=1.0">
+    <title>Vida Melhor</title>
+</head>
+<body>
+    <header>
+        <h1>Aqui será meu cabeçalho</h1>
+    </header>
