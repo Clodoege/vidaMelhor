4a5a01603657480a0eecd14e1241f69e07fb45eb
Clodoege <clodoege@gmail.com>
2024-04-12 19:08:38 -0300

    aulaAdministrativoVidaMelhor

--- adm/index.php
+++ adm/index.php
@@ -1,15 +1,26 @@
 <?php
 include 'inc/header.inc.php';
-?>  
-   
-    <h1>Aqui será o corpo do site</h1>
+?>
+
+<h2>ADMINISTRATIVO</h2>
+
+<div>
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <h1>Vida Melhor</h1>
+            <h1>Selecione a opção desejada</h1>
+        </div>
+
+        <ul>
+            <li><a href="gestao_exercicios.php">Gestão área exercícios</a></li>
+            <li><a href="gestao_foto_exercicio.php">Gestão fotos exercícios</a></li>
+            <li><a href="gestão_videos_exercicios.php">Gestão vídeos exercícios</a></li>
+            <li><a href="gestao_usuarios.php">Gestão de usuários</a></li>
+        </ul>
+    </div>
+
+</div>
 
 <?php
 include 'inc/footer.inc.php';
-
-
 ?>
-
-
-   
-    
---
+++ adm/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Vida Melhor</title>
+</head>
+<form method="POST" action="index.php">
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" id="senha" class="form-control">
+    </div>
+    <input type="submit" value="Fazer login" class= "btn btn-default">
+
+</form>
+
+
+
+
+
+
+
