74f1a7600e84a1473e9687f00b2f62667a8f9044
Clodoege <clodoege@gmail.com>
2024-05-08 22:30:42 -0300

    atualização08-05-24

--- adm/classes/exercicios.class.php
+++ adm/classes/exercicios.class.php
@@ -16,7 +16,7 @@ class Exercicios
         $this->nome = $nome;
         $this->categoria = $categoria;
 
-        $sql = $this->con->conectar()->prepare("INSERT INTO exercicio(nome, categoria)values(:nome, :categoria)");
+        $sql = $this->con->conectar()->prepare("INSERT INTO exercicio(nome, categoria)values(:nome, :categoria)");   //parei aqui em 02/05/2024
 
 
     }
